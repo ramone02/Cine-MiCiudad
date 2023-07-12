e7b18f09c9b2470f7e61d3acd57af78ce59118dd
ramone02 <leobellido@gmail.com>
2023-07-11 21:14:45 -0300

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Navigation } from "./Routes/Navigation";
+
+function App() {
+
+  return (
+    <>
+      <h1>Cine Mi Ciudad</h1>
+      <Navigation />
+    </>
+  );
+}
+
+export default App;
