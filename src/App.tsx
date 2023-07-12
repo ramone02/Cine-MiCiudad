db806980563fff147c1204df9909b7e493aec0d8
ramone02 <leobellido@gmail.com>
2023-07-11 23:44:31 -0300

    add: tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import { Navigation } from "./Routes/Navigation";
 
 function App() {
-
   return (
     <>
-      <h1>Cine Mi Ciudad</h1>
-      <Navigation />
+      <Navigation/>
     </>
   );
 }
--- src/Routes/Navigation.tsx
+++ src/Routes/Navigation.tsx
@@ -1,11 +1,30 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
 
 export const Navigation = () => {
   return (
     <BrowserRouter>
-      <div></div>;
+      <div className="bg-violet-900">
+        <nav>
+          <ul>
+            <NavLink
+              to="/"
+              className={(isActive) => (isActive ? "is-active" : "")}
+            >Home</NavLink>
+            <NavLink
+              to="/estrenos"
+              className={(isActive) => (isActive ? "is-active" : "")}
+            >Estrenos</NavLink>
+            <NavLink
+              to="/faq's"
+              className={(isActive) => (isActive ? "is-active" : "")}
+            >Faq's</NavLink>
+          </ul>
+        </nav>
+      </div>
       <Routes>
-        <Route />
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/estrenos" element={<h1>Estrenos</h1>} />
+        <Route path="/faqs" element={<h1>Faq's</h1>} />
       </Routes>
     </BrowserRouter>
   );
